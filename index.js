f31b75a1fc3417077d008a7820d7e999a5482cce
Lauri Viitanen <lauri.viitanen@tuni.fi>
2022-02-23 20:39:50 +0200

    3.19 and 3.20 done

--- index.js
+++ index.js
@@ -95,7 +95,11 @@ app.put(`${basePath}/:id`, (request, response, next) => {
 
   console.log(person)
 
-  Person.findByIdAndUpdate(request.params.id, person, { new: true })
+  Person.findByIdAndUpdate(
+    request.params.id, 
+    { name: body.name, number: body.number}, 
+    { new: true, runValidators: true, context: 'query' }
+  )
     .then(updatedPerson => response.json(updatedPerson))
     .catch(error => next(error))
 })
@@ -124,6 +128,9 @@ const errorHandler = (error, request, response, next) => {
   console.error(`[ERROR] ${error.message}`)
   if (error.name === 'CastError') {
     return response.status(400).send({ error: 'malformatted id' })
+  } 
+  else if (error.name === 'ValidationError') {
+    return response.status(400).json({ error: error.message })
   }
 
   next(error)
--- model/person.js
+++ model/person.js
@@ -1,10 +1,21 @@
 const mongoose = require('mongoose')
 
+const numberValidator = number => /\d{2}-\d{6}\d*/.test(number) || /\d{3}-\d{5}\d*/.test(number)
 
 // Define schema used
 const personSchema = new mongoose.Schema({
-	name: String,
-	number: String
+	name: {
+		type: String,
+		minlength: 3
+	},
+	number: {
+		type: String,
+		minlength: 8,
+		validate: {
+			validator: numberValidator,
+			message: "Number must be at least 8 numbers long. It shoud start with 2 or 3 numbers followed by dash (-) and the rest"
+		}
+	}
 })
 
 personSchema.set('toJSON', {
